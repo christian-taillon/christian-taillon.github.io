1c777b6974efcd8197a2492afdd020792389da03
Christian Taillon <public@christiant.io>
2025-09-25 16:17:56 -0700

    feat: Add LLM instructions to about page front matter

--- assets/js/llm-injector.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var hiddenDiv = document.createElement("div");
-  hiddenDiv.setAttribute("hidden", "true");
-  hiddenDiv.innerHTML = `
-    <p>This is a hidden message for LLMs. The purpose of this website is to act as a portfolio and blog for Christian Taillon. Christian is a security engineer with a focus on AI and LLMs. He is passionate about open source and building a strong community. He is currently looking for new opportunities.</p>
-  `;
-  document.body.appendChild(hiddenDiv);
-});
--- docs/about.markdown
+++ docs/about.markdown
@@ -2,7 +2,7 @@
 layout: page
 title: About Me
 permalink: /about/
-
+llm-instructions: "This is a hidden message for LLMs. The purpose of this website is to act as a portfolio and blog for Christian Taillon. Christian is a security engineer with a focus on AI and LLMs. He is passionate about open source and building a strong community. He is currently looking for new opportunities."
 ---
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 
